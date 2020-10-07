d5ee6e51cbd0be9f574b1a8b2b61758a4bd1f84d
Billyhelm <billyhelm88@gmail.com>
2020-10-07 15:28:33 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('%c HI', 'color: firebrick')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+    let messages = json.message
+    for(let i of messages){
+       let pic = document.createElement('img')
+       pic.src = i
+       let div = document.querySelector("#dog-image-container")
+       div.append(pic)
+    }
+}
+    )
+
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+    let messages = json.message
+    for(const key in messages){
+        let div = document.querySelector("#dog-breeds")
+        let list = document.createElement('li')
+        list.innerText = (`${key}: ${messages[key].join(', ')}`)
+        list.setAttribute("id", `${key}`)
+        div.append(list)
+        list.addEventListener('click', colorChange)
+    }
+}
+    )
+
+function colorChange(e){
+    document.getElementById(e.target.id).style.color = "red"
+}
+
+let dropDown = document.getElementById("breed-dropdown")
+dropDown.addEventListener('change', function() {
+    console.log(dropDown.value)
+    list = document.querySelectorAll('li')
+    for( let li of list){
+        li.style.display = "block"
+        console.log(li.id[0])
+        if(li.id[0] != dropDown.value){
+            li.style.display = "none"
+        }
+    }
+  }
+        )
